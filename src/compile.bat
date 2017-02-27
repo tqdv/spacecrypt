52fead185201326edf749255a0d94d653527534c
Baptiste <hemispace@outlook.com>
2017-02-27 22:02:30 +0100

    Release correcting

--- src/compile.bat
+++ src/compile.bat
@@ -1,2 +1,2 @@
-gcc clist.h clist.c crypt.h crypt.c fun.h fun.c methods.h methods.c main.c -o spacecrypt.exe
+gcc -m32 clist.h clist.c crypt.h crypt.c fun.h fun.c methods.h methods.c main.c -o spacecrypt.exe
 pause
